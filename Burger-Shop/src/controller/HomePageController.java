9e7c5ac72a6868e66407f82a721566ffc71641ed
DilshanWarnasuriya <dilshan.se.dew@gmail.com>
2024-03-28 00:45:05 +0530

    Create Home Page

---
+++ Burger-Shop/src/controller/HomePageController.java
@@ -0,0 +1,28 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+public class HomePageController {
+
+    public Button btnPlaceOrder;
+    public Button btnSearch;
+    public Button btnViewOrder;
+    public Button btnUpdateOrder;
+    public Button btnExit;
+
+    public void PlaceOrderOnAction(ActionEvent actionEvent) {
+    }
+
+    public void searchOnAction(ActionEvent actionEvent) {
+    }
+
+    public void ViewOrderOnAction(ActionEvent actionEvent) {
+    }
+
+    public void updateOrderOnAction(ActionEvent actionEvent) {
+    }
+
+    public void exitOnAction(ActionEvent actionEvent) {
+    }
+}
